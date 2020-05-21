acea687ce4556053820d4073d0afe4be7e6e0923
gmastergreatee <g77471164@gmail.com>
2020-05-21 19:15:23 +0530

    [Basic Move and Dodge Game] More refined player controls

--- Basic Move and Dodge Game/scripts/Player.gd
+++ Basic Move and Dodge Game/scripts/Player.gd
@@ -3,7 +3,7 @@ extends KinematicBody
 var velocity = Vector3(0,0,0)
 export var Speed = 5;
 export var MaxSpeed = 200;
-export(float, 0, 1) var Damp = 0.1
+export(float, 0, 10) var Friction = 0.1
 export(int, 1, 360) var RotateSpeed = 30;
 
 func _ready() -> void:
@@ -13,26 +13,26 @@ func _physics_process(delta: float) -> void:
 	
 #	process inputs
 	if Input.is_action_pressed("ui_left") and Input.is_action_pressed("ui_right"):
-		velocity.x = lerp(velocity.x, 0, Damp)
+		velocity.x = reset_move(delta, velocity.x)
 	elif Input.is_action_pressed("ui_left"):
-		velocity.x -= Speed * delta
+		velocity.x -= Speed * delta * Friction
 		rotate_z(deg2rad(RotateSpeed))
 	elif Input.is_action_pressed("ui_right"):
-		velocity.x += Speed * delta
+		velocity.x += Speed * delta * Friction
 		rotate_z(-deg2rad(RotateSpeed))
 	else:
-		velocity.x = lerp(velocity.x, 0, Damp)
+		velocity.x = reset_move(delta, velocity.x)
 	
 	if Input.is_action_pressed("ui_up") and Input.is_action_pressed("ui_down"):
-		velocity.z = lerp(velocity.z, 0, Damp)
+		velocity.z = reset_move(delta, velocity.z)
 	elif Input.is_action_pressed("ui_up"):
-		velocity.z -= Speed * delta
+		velocity.z -= Speed * delta * Friction
 		rotate_x(-deg2rad(RotateSpeed))
 	elif Input.is_action_pressed("ui_down"):
-		velocity.z += Speed * delta
+		velocity.z += Speed * delta * Friction
 		rotate_x(deg2rad(RotateSpeed))
 	else:
-		velocity.z = lerp(velocity.z, 0, Damp)
+		velocity.z = reset_move(delta, velocity.z)
 	
 	velocity.x = max(min(velocity.x, MaxSpeed), -MaxSpeed)
 	velocity.z = max(min(velocity.z, MaxSpeed), -MaxSpeed)
@@ -44,7 +44,10 @@ func _physics_process(delta: float) -> void:
 		velocity.y = -Speed
 	
 
-
+func reset_move(delta: float, vel: float) -> float:
+	if vel != 0:
+		vel = lerp(vel, 0, delta * Friction)
+	return vel
 
 
 
