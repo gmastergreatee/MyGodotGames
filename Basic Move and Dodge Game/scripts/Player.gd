6995aefa6f8cd646e92318f3eb78ded5296a51db
gmastergreatee <g77471164@gmail.com>
2020-05-21 18:41:10 +0530

    [Basic Move and Dodge Game] Added rotation & weight to player

--- Basic Move and Dodge Game/scripts/Player.gd
+++ Basic Move and Dodge Game/scripts/Player.gd
@@ -2,37 +2,41 @@ extends KinematicBody
 
 var velocity = Vector3(0,0,0)
 export var Speed = 5;
+export var MaxSpeed = 200;
 export(float, 0, 1) var Damp = 0.1
+export(int, 1, 360) var RotateSpeed = 30;
 
 func _ready() -> void:
 	pass
 
 func _physics_process(delta: float) -> void:
-	var rot_x = 0
-	var rot_z = 0
 	
 #	process inputs
 	if Input.is_action_pressed("ui_left") and Input.is_action_pressed("ui_right"):
-		velocity.x = 0
+		velocity.x = lerp(velocity.x, 0, Damp)
 	elif Input.is_action_pressed("ui_left"):
-		velocity.x = -Speed
-		
+		velocity.x -= Speed * delta
+		rotate_z(deg2rad(RotateSpeed))
 	elif Input.is_action_pressed("ui_right"):
-		velocity.x = Speed
+		velocity.x += Speed * delta
+		rotate_z(-deg2rad(RotateSpeed))
 	else:
 		velocity.x = lerp(velocity.x, 0, Damp)
 	
 	if Input.is_action_pressed("ui_up") and Input.is_action_pressed("ui_down"):
-		velocity.z = 0
+		velocity.z = lerp(velocity.z, 0, Damp)
 	elif Input.is_action_pressed("ui_up"):
-		velocity.z = -Speed
+		velocity.z -= Speed * delta
+		rotate_x(-deg2rad(RotateSpeed))
 	elif Input.is_action_pressed("ui_down"):
-		velocity.z = Speed
+		velocity.z += Speed * delta
+		rotate_x(deg2rad(RotateSpeed))
 	else:
 		velocity.z = lerp(velocity.z, 0, Damp)
-		
 	
-		
+	velocity.x = max(min(velocity.x, MaxSpeed), -MaxSpeed)
+	velocity.z = max(min(velocity.z, MaxSpeed), -MaxSpeed)
+	
 #	move the player
 	velocity = move_and_slide(velocity, Vector3.UP)
 	if not is_on_floor():
